070004a151b334a0389e4072c5fa92107c2df860
Juan Diego <172089930+jdpadillavigo@users.noreply.github.com>
2025-05-04 14:19:00 -0500

    Project created

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { Routes, Route} from 'react-router-dom'
+
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home'
+// import Explore from './pages/Explore/Explore'
+// import Categories from './pages/Categories/Categories'
+// import Platform from './pages/Platform/Platform'
+// import SpecialOffers from './pages/SpecialOffers/SpecialOffers'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          {/* <Route path='/explorar' element={<Explore />} />
+          <Route path='/categorias' element={<Categories />} />
+          <Route path='/plataforma' element={<Platform />} />
+          <Route path='/ofertas' element={<SpecialOffers />} /> */}
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
